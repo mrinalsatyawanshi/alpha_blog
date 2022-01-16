a51eafdd7d61c47c14cc2c04de2ad20c32f1dca7
mrinalsatyawanshi <mrinal.satyawanshi@anchanto.com>
2022-01-16 13:47:25 +0530

    Add edit and update feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,32 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @articles = Article.new
+    end
+
+    def edit
+        @articles = Article.find(params[:id])
+    end
+
+    def create
+        @articles = Article.new(params.require(:article).permit(:title, :description))
+        if @articles.save
+            flash[:notice] = "Article created Successfully!!"
+            redirect_to @articles
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @articles = Article.find(params[:id])
+        if @articles.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article updated Successfully!!"
+            redirect_to @articles
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
 end
